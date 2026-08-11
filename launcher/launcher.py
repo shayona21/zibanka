8264200fc5e40878497926aff2b569ce11f68679
shayona21 <shayona@uchicago.edu>
2026-08-11 19:40:58 +0800

    fixing launcher

--- launcher/launcher.py
+++ launcher/launcher.py
@@ -66,6 +66,4 @@ def logo():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "5050"))
-    host = os.environ.get("HOST", "0.0.0.0")
-    app.run(host=host, port=port, debug=False)
+    app.run(host="0.0.0.0", port=5050)
